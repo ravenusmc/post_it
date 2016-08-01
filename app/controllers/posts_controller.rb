97ecee7de1183c4750756a943a66794fb69c66be
ravenusmc <mcuddy77@gmail.com>
2016-08-01 14:12:51 -0400

    added a new and create actions on the controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,24 @@ class PostsController < ApplicationController
   def show 
     @post = Post.find(params[:id])
   end
+
+  def new 
+    @post = Post.new 
+  end 
+
+  def create 
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post), notice: "Post Created!"
+    else 
+      render :new, alert: "Error Creating Post!"
+    end
+  end
+
+  private 
+
+    def post_params
+      params.require(:post).permit(:description, :avatar)
+    end
   
 end
