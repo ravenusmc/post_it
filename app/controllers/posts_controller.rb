3bb371770ac03fd7f08371ef574d56b6ca256c65
ravenusmc <mcuddy77@gmail.com>
2016-08-02 13:35:04 -0400

    added edit and update actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,9 +21,16 @@ class PostsController < ApplicationController
   end
 
   def edit 
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to @post
+    else 
+      'Edit'
+    end
   end
 
   def destroy
