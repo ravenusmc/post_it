3ed6d4ba4ff0e1bbede02375ed24a1549b55cbb2
ravenusmc <mcuddy77@gmail.com>
2016-08-05 14:09:30 -0400

    added validations for the model and added new and create actions for comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,32 @@
 class CommentsController < ApplicationController
 
   def index 
+    @post = Post.find(params[:post_id])
   end 
 
   def show 
     @comment = Comment.find(params[:id])
   end 
+
+  def new 
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new 
+  end
+
+  def create 
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    if @comment.save
+      redirect_to @post
+    else 
+      render 'new'
+    end 
+  end 
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:name, :description, :post_id)
+    end
 
 end
